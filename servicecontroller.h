792a9b13c99d613b406c48cb9a47230df8af1be8
Windows wang <wangmingzhang@outlook.com>
2017-07-24 17:05:48 +0800

    加入ServiceController；在安装服务之后，设置服务为手动启动

---
+++ servicecontroller.h
@@ -0,0 +1,11 @@
+#ifndef SERVICECONTROLLER_H
+#define SERVICECONTROLLER_H
+
+
+class ServiceController : public QtServiceController
+{
+public:
+    ServiceController();
+};
+
+#endif // SERVICECONTROLLER_H
